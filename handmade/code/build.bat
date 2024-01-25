266b5a68da33473e4b33d797f67bd3e93f20eb43
brunosartori-agile <bruno.sartori@agilecontent.com>
2024-01-25 17:49:44 -0300

    Tutorial 02 - Opening a Win32 window

--- handmade/code/build.bat
+++ handmade/code/build.bat
@@ -4,5 +4,5 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib
+cl -Zi ..\handmade\code\win32_handmade.cpp user32.lib gdi32.lib
 popd 
--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,6 +1,106 @@
 #include <windows.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    MessageBox(0, "Hello World", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch (Message)
+    {
+    case WM_PAINT:
+    {   
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        int X = Paint.rcPaint.left;
+        int Y = Paint.rcPaint.top;
+        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+
+        PatBlt(DeviceContext, X, Y, Width, Height, WHITENESS);
+        EndPaint(Window, &Paint);
+    } break;
+    case WM_SIZE:
+    {
+        OutputDebugStringA("WM_SIZE\n");
+    }
+    break;
+
+    case WM_DESTROY:
+    {
+        OutputDebugStringA("WM_DESTROY\n");
+    }
+    break;
+
+    case WM_CLOSE:
+    {
+        OutputDebugStringA("WM_CLOSE\n");
+    }
+    break;
+
+    case WM_ACTIVATEAPP:
+    {
+        OutputDebugStringA("WM_ACTIVATEAPP\n");
+    }
+    break;
+
+    default:
+    {
+        // OutputDebugStringA("DEFAULT\n");
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+    }
+    break;
+    }
+
+    return Result;
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    // MessageBox(0, "Hello World", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+
+    WNDCLASS WindowClass = {};
+    WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = hInstance;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+    // WindowClass.hIcon;
+
+    if (RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(
+            0,
+            WindowClass.lpszClassName,
+            "Handmade Hero",
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            0,
+            0,
+            hInstance,
+            0
+        );
+
+        if (WindowHandle) {
+            MSG Message;
+            for (;;) {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+
+                if (MessageResult > 0) {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO: log
+        }
+    }
+    else
+    {
+        // TODO: log
+    }
+
     return (0);
 }
